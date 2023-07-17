ee13e550c850ad45256887ef1f33ecd293f8c667
Liplan Lekipising <leeplanke@gmail.com>
2023-07-17 23:33:49 +0200

    setup API routes

--- src/services/apiHooks.ts
+++ src/services/apiHooks.ts
@@ -1,34 +1,70 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseUrl: process.env.NEXT_PUBLIC_API_URL,
   credentials: "include",
-  prepareHeaders: (headers, { getState }) => {
-    // @ts-ignore
-    const { token } = getState().auth;
-    if (token) {
-      headers.set("authorization", `Bearer ${token}`);
-    }
-    return headers;
-  },
 });
 
 export const apiHooks = createApi({
   baseQuery,
   tagTypes: ["Dashboard", "Invoices", "Payouts"],
   endpoints: (builder) => ({
-    joinWaitlist: builder.mutation({
-      query: (email) => ({
-        url: `waitlist`,
-        method: "POST",
-        body: {
-          email,
-        },
+    getInvoices: builder.query({
+      query: (accountId, page = 1) => ({
+        url: `payment-invoice?accountId=${accountId}&page=${page}`,
+      }),
+    }),
+    getPayouts: builder.query({
+      query: (accountId, page = 1) => ({
+        url: `settlement-transaction?accountId=${accountId}&page=${page}`,
+      }),
+    }),
+    getPaymentAccounts: builder.query({
+      query: (accountId) => ({
+        url: `payment-account?accountId=${accountId}`,
+      }),
+    }),
+    getTotalPaidInvoices: builder.query({
+      query: (accountId) => ({
+        url: `account-statistics/total-paid-invoices/${accountId}`,
+      }),
+    }),
+    getTranscationValueLast7Days: builder.query({
+      query: (accountId) => ({
+        url: `account-statistics/total-transaction-value-last-7-days/${accountId}`,
+      }),
+    }),
+    getTranscationValueLast30Days: builder.query({
+      query: (accountId) => ({
+        url: `account-statistics/total-transaction-value-last-30-days/${accountId}`,
+      }),
+    }),
+    averageTranscationValueDaily: builder.query({
+      query: (accountId) => ({
+        url: `account-statistics/average-daily-transaction-value/${accountId}`,
+      }),
+    }),
+    getBalance: builder.query({
+      query: (accountId) => ({
+        url: `account-statistics/balance/${accountId}`,
+      }),
+    }),
+    getProjectedBalance: builder.query({
+      query: (accountId) => ({
+        url: `account-statistics/projected-balance/${accountId}`,
       }),
     }),
   }),
 });
 
 export const {
-  useJoinWaitlistMutation,
+  useGetInvoicesQuery,
+  useGetPayoutsQuery,
+  useGetPaymentAccountsQuery,
+  useGetTotalPaidInvoicesQuery,
+  useGetTranscationValueLast7DaysQuery,
+  useGetTranscationValueLast30DaysQuery,
+  useAverageTranscationValueDailyQuery,
+  useGetBalanceQuery,
+  useGetProjectedBalanceQuery,
 } = apiHooks;
